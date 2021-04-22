aac407fc2a4efe6fc4937e02b6055801991962dd
Matt Roden <mattroden92@gmail.com>
2021-04-22 11:03:43 -0600

    Add vehicle models to model.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  make: "Ford",
+  model: "Fusion",
+  miles: 45000,
+  numberOfDoors: 4,
+  isDamaged: false,
+  isClean: true,
+  features: ['Bluetooth stereo', 'All wheels drive', 'Backup camera system'],
+  bodyMaterials: ['sheet metal', 'plastic', 'chrome']
+}
