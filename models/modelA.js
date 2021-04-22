bca06b32886deb289b048c1f1d3b976c9515bfd0
Matt Roden <mattroden92@gmail.com>
2021-04-22 10:59:23 -0600

    Add modelA.js and modelB.js + modelA airport model

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var airport = {
+  name: "Denever International Arport",
+  nickname: "DIA",
+  numOfTerminals: 3,
+  numOfGates: 100,
+  isMassive: true,
+  isHaunted: true,
+  terminalNames: ['Terminal A', 'Temrinal B', 'Temrinal C'],
+  conspiracies: ['Lizard people', 'Underground government tunnels', 'Bluecifer']
+}
+ console.log(airport);
